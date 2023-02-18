ea759fcdd4a03f5080e70aebfe9bf4f0dbbb017c
js06283 <jessicashi605@gmail.com>
2023-02-18 15:59:13 -0800

    change display

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -5,7 +5,7 @@ export const Display = () => {
         <div className = "flex flex-col flex-grow bg-stone-200">
             <div className="p-2 text-lg">Dashboard</div>
             <div className="flex h-[50%] bg-green-300 m-8 mt-2 rounded-xl">
-                
+            
             </div>
             <Simulation /> 
         </div> 
