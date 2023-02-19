9bb9bbb58d138f4b3ed32f135d44822830b3e6bb
Sarah Gu <guwen888@gmail.com>
2023-02-18 17:43:12 -0800

    eslint fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { Montserrat } from '@next/font/google'
 
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL)
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL ?? "")
 const monty = Montserrat({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }: AppProps) {
