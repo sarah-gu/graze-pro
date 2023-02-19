933f2e97930538ebda0dd33dbc1fa430ced77ea2
js06283 <jessicashi605@gmail.com>
2023-02-18 20:17:38 -0800

    Fix package dependency for html-loader

--- next.config.js
+++ next.config.js
@@ -1,2 +1,13 @@
-const withHtml = require('@blunck/next-html')()
-module.exports = withHtml()
+module.exports = {
+    webpack: (
+      config,
+      { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    ) => {
+      // Important: return the modified config
+      config.module.rules.push({
+        test: /\.html$/i,
+        loader: "html-loader",
+      });
+      return config
+    },
+  }
